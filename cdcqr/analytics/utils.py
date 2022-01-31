fbaa2cfa2ac21c8aebe6f290c3d7e3f5733c4eee
Wang <wang.han@crypto.com>
2022-01-31 06:11:15 +0000

    Revert "Revert "funding rate data""
    This reverts commit b2f57cfcfdcb3882bed3716f29d415d08de9848d.

--- cdcqr/analytics/utils.py
+++ cdcqr/analytics/utils.py
@@ -8,6 +8,8 @@ import seaborn as sns
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 from numpy.lib import pad
+import os
+from cdcqr.common.config import LOCAL_FIGURE_DIR
 
 
 
@@ -81,69 +83,78 @@ def rolling_rank_corr(df, window, y = None, min_periods=1):
         return pd.DataFrame(ret_dict, index=df.index)
 
 
-def ts_eda(dff0, x, xn, y, lbw = 20):
+def ts_eda(dff0, x, xn, y, lbw = 50, save_fig=False):
     dff = dff0.copy()
     # double y plot
-    cols = dff.columns
 
     dff['xn_pctrank'] = dff[xn].rolling(lbw).apply(pctrank)
     dff['xn_zscore'] = zscore(dff[xn], lbw)
 
     dff.plot2(x, y)
-    dff.plot2(xn, y)
-    dff.plot2('xn_pctrank', y)
-    dff.plot2('xn_zscore', y)
+    #dff.plot2(xn, y)
+    #dff.plot2('xn_pctrank', y)
+    #dff.plot2('xn_zscore', y)
 
     n_f = 3
-    n_lag = 3
+    n_fret = 3
     n_group = 4
-    fig = plt.figure(num=1, figsize=(6 * n_f, 4 * n_lag*n_group))
+    fig = plt.figure(num=1, figsize=(6 * n_f, 4 * n_fret*n_group))
     dff['yr'] = dff[y].pct_change()
-
+    dff['yr+10'] = dff[y].pct_change(periods=10).shift(-10)
+    dff['yr+100'] = dff[y].pct_change(periods=100).shift(-100)
+    dff['yr+1000'] = dff[y].pct_change(periods=1000).shift(-1000)
     for i, feature in enumerate([xn,'xn_pctrank','xn_zscore']):
-        for j, lag in enumerate([0,1,2]):
-            dff['f_lagged'] = dff[feature].shift(lag)
+        for j, forward_ret in enumerate(['yr+10','yr+100','yr+1000']):
+            
             lower_bound1 = dff[xn].quantile(0.05)
             upper_bound1 = dff[xn].quantile(0.95)
             lower_bound2 = dff[xn].quantile(0.01)
             upper_bound2 = dff[xn].quantile(0.99)
 
-            mid90 = dff[(dff[xn] < upper_bound1) & (dff[xn] > lower_bound1)]
-            outlier8 = dff[((dff[xn] >= upper_bound1) & (dff[xn] < upper_bound2)) | (
-                        (dff[xn] <= lower_bound1) & (dff[xn] > lower_bound2))]
-            outlier2 = dff[(dff[xn] >= upper_bound2) | (dff[xn] <= lower_bound2)]
 
             upper95_99 = dff[((dff[xn] >= upper_bound1) & (dff[xn] < upper_bound2))]
             upper99 = dff[(dff[xn] >= upper_bound2)]
             lower1 = dff[(dff[xn] <= lower_bound2)]
-            lower1_5 = dff[((dff[xn] >= lower_bound2) & (dff[xn] < lower_bound1))]
-
+            lower1_5 = dff[((dff[xn] > lower_bound2) & (dff[xn] <= lower_bound1))]
+            #print(lower_bound1, lower_bound2, upper_bound1, upper_bound2)
+            #print(lower1.shape, lower1_5.shape, upper95_99.shape, upper99.shape)
             group_dict={}
             group_dict[0] = 'lower1'
             group_dict[1] = 'lower1_5'
             group_dict[2] = 'upper95_99'
             group_dict[3] = 'upper99'
             for k, data in enumerate([lower1, lower1_5, upper95_99, upper99]):
-                ax = fig.add_subplot(n_lag * n_group, n_f, i * n_lag * n_group + j * n_group + k + 1)
+                ax = fig.add_subplot(n_fret * n_f, n_group, i * n_fret * n_group + j * n_group + k + 1)
                 #data.plot.scatter(x='f_lagged', y='yr', c='DarkBlue', s=2, ax=ax)
 
-                dflr = data[[feature,'yr']].dropna()
+                dflr = data[[feature, forward_ret]].dropna()
 
-                x1 = '{}_lag{}_{}'.format(feature, lag, group_dict[k])
-                y1 = 'yr'
+                x1 = '{}_{}'.format(feature, group_dict[k])
+                y1 = forward_ret
+                f_ret = dflr[y1]
                 dflr.columns = [x1, y1]
-                lr=LinearRegression(fit_intercept=False).fit(dflr[[x1]],dflr[y1])
-                lri=LinearRegression(fit_intercept=True).fit(dflr[[x1]],dflr[y1])
-                sns.scatterplot(x=x1,y=y1,data=dflr, ax = ax)
-                ax.plot(dflr[x1],lr.predict(dflr[[x1]]),color='g')
-                ax.plot(dflr[x1],lri.predict(dflr[[x1]]),color='r')
-                plt.title(f"r {lr.coef_[0]:.3f}x R2 {r2_score(dflr[[y1]],lr.predict(dflr[[x1]])):.3f} g {lri.coef_[0]:.3f}x+{lri.intercept_:.3f} R2 {r2_score(dflr[[y1]],lri.predict(dflr[[x1]])):.3f}")
+                if dflr.shape[0]>0:
+                    lr=LinearRegression(fit_intercept=False).fit(dflr[[x1]],f_ret)
+                    lri=LinearRegression(fit_intercept=True).fit(dflr[[x1]],f_ret)
+                    sns.scatterplot(x=x1,y=y1,data=dflr, ax = ax)
+                    ax.plot(dflr[x1],lr.predict(dflr[[x1]]),color='g')
+                    ax.plot(dflr[x1],lri.predict(dflr[[x1]]),color='r')
+                    plt.title(f"r {lr.coef_[0]:.3f}x R2={r2_score(dflr[[y1]],lr.predict(dflr[[x1]])):.3f} \
+                        \n g {lri.coef_[0]:.3f}x+{lri.intercept_:.3f} R2={r2_score(dflr[[y1]],lri.predict(dflr[[x1]])):.3f} \
+                        \n E(r)={dflr[y1].mean():.5f}")
+                else:
+                    sns.scatterplot(x=x1,y=y1,data=dflr, ax = ax)
+                    plt.title('No data available')
 
 
                 #plt.title('{} lag {} {} v.s. {}'.format(feature, lag, group_dict[k],'{} return'.format(y)), )
 
-        fig.tight_layout()
-
+    fig.tight_layout()
+    if save_fig:
+        fig_name = '{}_{}'.format(x,y)
+        fig_path = os.path.join(LOCAL_FIGURE_DIR, '{}.png'.format(fig_name))
+        fig.savefig(fig_path)    
+        print('{} saved'.format(fig_path))
 
     if True:
         # return scatter plots
